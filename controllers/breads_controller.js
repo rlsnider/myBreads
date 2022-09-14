91c711c0cdd69caadae6d608f40308ef0e5736ca
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-09-14 12:05:30 -0700

    made 404 stubs, and show stub

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -7,14 +7,18 @@ breads.get('/', (req, res)=>{
     res.render('index', {
         breads: Bread,
         title: 'Index Page'
-    }
+    }                               
     )
-   // res.send(Bread)
 })
-
-
+//SHOW 
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
+    if (Bread[req.params.arrayIndex]) {
+        res.render('Show', {
+            bread:Bread[req.params.arrayIndex]
+        })
+    } else {
+        res.send('404')
+    }
 })
 
 module.exports = breads
