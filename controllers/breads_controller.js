80689671ee550f0f3ec8b5e1471d36be8031d8d1
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-09-18 11:21:34 -0700

    added edit and update routes

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,7 +4,8 @@ const Bread = require('../models/bread.js')
 
 //INDEX
 breads.get('/', (req, res)=>{
-    res.render('index', {
+  //Changed Index to upperCase
+    res.status(303).render('Index', {
         breads: Bread,
         title: 'Index Page'
     }                               
@@ -13,20 +14,10 @@ breads.get('/', (req, res)=>{
 
 //NEW
 breads.get('/new', (req, res) => {
-    res.render('new');
+    res.status(303).render('new');
 })
 
-//SHOW 
-breads.get('/:arrayIndex', (req, res) => {
-    if (Bread[req.params.arrayIndex]) {
-        res.render('Show', {
-            bread:Bread[req.params.arrayIndex],
-            index: req.params.arrayIndex,
-        })
-    } else {
-        res.render('error404')
-    }
-})
+
 // CREATE
 breads.post('/', (req, res) => {
     if (!req.body.image) {
@@ -38,8 +29,47 @@ breads.post('/', (req, res) => {
       req.body.hasGluten = false
     }
     Bread.push(req.body)
-    res.redirect('/breads')
+    res.status(303).redirect('/breads')
   })
+
+
+//EDIT 
+breads.get('/:indexArray/edit', (req, res) => {
+  console.log('you have reached the /Edit')
+  console.log(req.params.indexArray)
+  console.log(Bread[req.params.indexArray])
+  res.status(303).render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
+  //SHOW 
+breads.get('/:arrayIndex', (req, res) => {
+  if (Bread[req.params.arrayIndex]) {
+      res.render('Show', {
+          bread:Bread[req.params.arrayIndex],
+          index: req.params.arrayIndex,
+      })
+  } else {
+      res.render('error404')
+  }
+})
+
+
+//UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  console.log('hit /Update')
+  console.log(Bread[req.params.arrayIndex])
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.status(303).redirect(`/breads/${req.params.arrayIndex}`)
+})
+
   //DELETE 
   breads.delete('/:indexArray', (req, res) => {
     Bread.splice(req.params.indexArray, 1)
--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 //ROUTES
 app.get('/', (req, res) => {
