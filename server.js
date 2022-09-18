ceb9e39fd2ecfd6ba34df9a17b084d3c347160f3
Ramona Snider <41329897+rlsnider@users.noreply.github.com>
2022-09-17 17:52:43 -0700

    delete route

--- server.js
+++ server.js
@@ -1,32 +1,34 @@
-const express = require('express')
+const express = require('express');
+const methodOverride = require('method-override');
 
 //CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
+require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
 
 //MIDDLEWARE
-app.set('views', __dirname + '/views')
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'))
 
 //ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads!')
-})
+    res.send('Welcome to an Awesome App about Breads!');
+});
  
 //Breads
-const breadsController = require('./controllers/breads_controller.js')
-app.use('/breads', breadsController)
+const breadsController = require('./controllers/breads_controller.js');
+app.use('/breads', breadsController);
 
 // ERROR ROUTE
 app.get('*', (req, res) => {
-    res.render('error404')
-})
+    res.render('error404');
+});
 
 //LISTEN
 app.listen(PORT, ()=>{
     console.log('listening on port', PORT);
-})
+});
